4187b36a3b1a88c423b9897f4c566bbd629168a4
teenspirittt <vshabunin25@gmail.com>
2023-07-20 02:13:38 +0700

    working spark sumbit operator

--- src/scala/DataProcessing.scala
+++ src/scala/DataProcessing.scala
@@ -2,28 +2,32 @@ import org.apache.spark.sql.SparkSession
 
 object DataProcessing {
   def main(args: Array[String]): Unit = {
+   
     val date = java.time.LocalDate.now().toString
-    val date_vacancies = date
-    val date_cleaned = date 
+
+    val date_vacancies = s"$date" + "_vacancies"
+    val date_cleaned = s"$date" + "_vacancies_cleaned"
 
     val spark = SparkSession.builder
-      .appName("DataProcessing")
-      .master("spark://sparkmaster:7077")
-      .config("spark.executor.memory", "1g")
-      .config("spark.driver.host", "spark-master")
+      .appName("DataProcessing") 
+      .master("spark://sparkmaster:7077") 
       .getOrCreate()
+
 
     val df = spark.read.json(s"hdfs://namenode:9000/hadoop-data/$date_vacancies.json")
 
-    val cleanedDF = df.dropDuplicates()
+   
+ val cleanedDF = df.dropDuplicates()
       .filter(df("salary.from") > 0)
       .withColumn("salary.from", df("salary.from").cast("float"))
       .drop("response_letter_required")
-      .na.drop("salary.from")
+      .na.drop(Seq("salary.from")) 
       .na.fill(0, Seq("salary.to"))
 
-    cleanedDF.write.parquet(s"hdfs://namenode:9000/hadoop-data/processed/$date_cleaned.parquet")
+   
+    cleanedDF.write.mode("overwrite").parquet(s"hdfs://namenode:9000/hadoop-data/$date_cleaned.parquet")
 
+    
     spark.stop()
   }
 }
