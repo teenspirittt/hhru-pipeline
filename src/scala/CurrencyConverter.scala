44f0c76bad93886aca765f006b03156aba6be874
teenspirittt <vshabunin25@gmail.com>
2023-10-05 19:40:15 +0700

    add currency converter

---
+++ src/scala/CurrencyConverter.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+import scala.xml.XML
+
+object CurrencyConverter {
+  def getCurrencyRate(currencyCode: String): Option[Double] = {
+    val url = s"https://www.cbr.ru/scripts/XML_daily.asp?date_req=01/10/2023" // Замените дату по желанию
+    val xmlString = Source.fromURL(url, "windows-1251").mkString
+
+    try {
+      val parsedXml = XML.loadString(xmlString)
+      val rateNode = (parsedXml \\ "Valute").find(v => (v \ "CharCode").text == currencyCode)
+      
+      rateNode match {
+        case Some(rate) =>
+          val nominal = (rate \ "Nominal").text.toInt
+          val rateValue = (rate \ "Value").text.replace(",", ".").toDouble
+          val oneRubToCurrency = rateValue / nominal
+          Some(oneRubToCurrency)
+        case None =>
+          None
+      }
+    } catch {
+      case e: Exception =>
+        println(s"Error when getting the exchange rate: ${e.getMessage}")
+        None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val currencyCode = "KZT"
+    getCurrencyRate(currencyCode) match {
+      case Some(rate) => println(s"Курс 1 рубля к $currencyCode: $rate")
+      case None => println(s"Курс $currencyCode не найден")
+    }
+  }
+}
